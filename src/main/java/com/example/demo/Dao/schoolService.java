b154416f089d90d25979300f83a2be7e5f99e82a
unknown <wp900622@gmail.com>
2022-07-26 23:49:43 +0800

    third

---
+++ src/main/java/com/example/demo/Dao/schoolService.java
@@ -0,0 +1,72 @@
+package com.example.demo.Dao;
+
+
+
+import com.example.demo.entity.school_register;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+@Component
+public class schoolService {
+    @Autowired
+    SchoolDao schooldao;
+    public Iterable<school_register> getregister() {
+        return schooldao.findAll();
+    }
+
+    public Integer createSchoolService(school_register school)  {
+        school_register rlt;
+        String pattern = "^([a-zA-Z0-9_\\-.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+        Pattern r = Pattern.compile(pattern);
+
+        if(school.getMail().trim().length() == 0 ||
+           !school.getMail().matches(pattern)){
+           return 1;
+        }
+        if(school.getUsername().trim().length() == 0){
+            return 2;
+        }
+        if(school.getPwd().trim().length() == 0){
+          return 3;
+        }
+        if(school.getSchool().trim().length() == 0){
+            return 4;
+        }else{
+            schooldao.save(school);
+            return 0;
+        }
+
+
+    }
+
+    public Boolean updateSchoolService(Integer Id, school_register school){
+        Optional<school_register> isExit = findByMail(Id);
+        if(! isExit.isPresent()){
+            return false;
+        }
+        school_register newschool = isExit.get();
+        if(school.getPwd() == null){
+            return false;
+        }
+        if(school.getUsername() == null){
+            return false;
+        }
+        newschool.setPwd(school.getPwd());
+        newschool.setUsername(school.getUsername());
+        newschool.setSchool(school.getSchool());
+        newschool.setMail(school.getMail());
+        schooldao.save(newschool);
+        return  true;
+    }
+
+    public Optional<school_register> findByMail(Integer Id) {
+        Optional<school_register> school = schooldao.findById(Id);
+        return school;
+    }
+}
