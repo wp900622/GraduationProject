ba10270848e9754dd7076be9a470e32fac358018
fcu-d0813191 <wp900622@gmail.com>
2022-09-06 14:57:22 +0800

    seventh

--- src/main/java/fcu/iecs/volunteer/controller/SchoolController.java
+++ src/main/java/fcu/iecs/volunteer/controller/SchoolController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 
-
+@CrossOrigin
 @RestController
 public class SchoolController {
     private static final Logger logger
