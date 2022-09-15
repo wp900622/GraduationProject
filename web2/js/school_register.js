3e37222dcde8da1411394e1b24ebd6fc42b6abe3
Sammy Chen <seeme.goo@gmail.com>
2022-09-15 17:08:46 +0800

    修改post方法，使用JQuery

--- web2/js/school_register.js
+++ web2/js/school_register.js
@@ -1,33 +1,41 @@
-function PostData(url , data){
-    return fetch(url,{
-        body: JSON.stringify(data),
-        
-        headers: {
-            'header-agent': 'Example',
-            'content-type' : 'application/json'
-        },
-        method: 'POST',
-       
-       
-    }).then(response=> response.json)
-}
-function submit(){
-    const validationServer01 = document.getElementById('validationServer01').value;
-    const validationServer02 = document.getElementById('validationServer02').value;
-    const validationServer03 = document.getElementById('validationDefault03').value;
-    const validationServer04 = document.getElementById('validationDefault04').value;
-    const validationServer05 = document.getElementById('validationDefault05').value;
-    const validationServer06 = document.getElementById('validationDefault06').value;
-    const validationServer07 = document.getElementById('validationDefault07').value;
-    const validationServer08 = document.getElementById('validationDefault08').value;
-    const data = {'school_account':validationServer01 ,'pwd': validationServer02,'school_account':validationServer03,'telephone':validationServer04,
-        'address_county':validationServer05,'address_district':validationServer06,'address':validationServer07,'mail':validationServer08}; 
-    console.log(data);
-   const uri = 'http://140.134.24.157:53008/schoolpost';
-    PostData(uri, data.String).then(
-        console.log(data.result)
-    )
-    
+const schoolRegApiUrl = 'http://140.134.24.157:53008/schoolpost';
 
-    
-}
+$(document).ready(function() {
+    $('#submitBtn').click(function() {
+        const account = $('#validationServer01').val();
+        const password = $('#validationServer02').val();
+        const schoolName = $('#validationDefault03').val();
+        const telNo = $('#validationDefault04').val();
+        const city = $('#validationDefault05').val();
+        const district = $('#validationDefault06').val();
+        const address = $('#validationDefault07').val();
+        const email = $('#validationDefault08').val();
+
+        const schoolInfo = {
+            school_account: account,
+            pwd: password,
+            school: schoolName, 
+            telephone: telNo,
+            address_county: city,
+            address_district: district,
+            address: address,
+            mail: email
+        }
+        console.log(schoolInfo);
+
+        $.ajax({
+            type: 'POST',
+            url: schoolRegApiUrl,
+            contentType: 'application/json;charset=utf-8',
+            data: JSON.stringify(schoolInfo),
+            dataType: 'json',
+
+            success: function(result) {
+                console.log(result);
+                if (result == 0) {
+                    alert("註冊成功");
+                }
+            },
+        });
+    });  
+});
